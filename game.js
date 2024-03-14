6952bd3e9b3a0b72c9c6bf4876c69a8fcc296d88
jbagge <jan.bagge94@gmail.com>
2024-03-14 22:37:22 +0200

    make it more fun

--- game.js
+++ game.js
@@ -13,35 +13,47 @@ const rl = readline.createInterface({ input, output });
 const reset = () => {
   digits = pi.split('');
   out = digits.splice(0,4).join('');
+  rl.write('\u001B[2J\u001B[0;0f');
+  rl.write('Continue the sequence!\n> 3.14');
 }
 
-rl.write('Continue the sequence!\n> 3.14');
 reset();
 
+let timer;
+let count = 0;
 input.on('keypress', (c) => {
-  rl.write(null, { ctrl: true, name: 'u' });
-  if ((c+'').match(/[^0-9rc]/)) {
+  if (timer) {
     return;
   }
-  if (c === 'r') {
-    rl.write(out);
-    rl.write('\n> ');
-    reset();
-    rl.write(out);
-    return;
-  }
-  if (c === 'c') {
-    const peep = digits.slice(0,10).join('');
-    rl.write(out + cs + peep + end);
-    digits.unshift(...peep.split(''));
+  rl.write('\u001B[2J\u001B[0;0f');
+  rl.write('Continue the sequence!\n> ');
+  rl.write(out);
+  // rl.write(null, { ctrl: true, name: 'u' });
+  if ((c+'').match(/[^0-9]/)) {
     return;
   }
   const next = digits.shift();
   if (c === next) {
-    out += gs + next;
-  } else {
-    out += rs + next;
+    rl.write(gs + next + end);
+    out += gs + next + end;
+    return;
   }
-  out += end;
-  rl.write(out);
+  const peep = digits.slice(0,10).join('');
+  rl.write(rs + next + cs + peep + end);
+  out += rs + next + cs + peep + end;
+  digits.unshift(...peep.split(''));
+  timer = setInterval(() => {
+    const ttl = 10;
+    rl.write('\u001B[2J\u001B[0;0f');
+    rl.write('Continue the sequence!\n> ');
+    rl.write(out + '\n> Resetting in ' + (ttl - count) + 's');
+    if (ttl - count === 0) {
+      clearInterval(timer);
+      timer = null;
+      count = 0;
+      reset();
+      return;
+    }
+    count += 1;
+  }, 1000);
 });
