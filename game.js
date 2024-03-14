5b1eaa253c5f1a067734381bb51ca7e5ec909dfd
jbagge <jan.bagge94@gmail.com>
2024-03-14 22:51:16 +0200

    make it even more fun

--- game.js
+++ game.js
@@ -10,42 +10,54 @@ const end = '\x1b[0m';
 
 const rl = readline.createInterface({ input, output });
 
+let max_mistakes = 5;
+
+const write = (out, mistakes, max_mistakes) => {
+  rl.write('\u001B[2J\u001B[0;0f');
+  rl.write(`Continue the sequence! mistakes ${mistakes}/${max_mistakes}\n> `);
+  rl.write(out);
+}
+
 const reset = () => {
   digits = pi.split('');
   out = digits.splice(0,4).join('');
-  rl.write('\u001B[2J\u001B[0;0f');
-  rl.write('Continue the sequence!\n> 3.14');
+  write(out, 0, max_mistakes);
 }
 
 reset();
 
 let timer;
 let count = 0;
+let mistakes = 0;
+
 input.on('keypress', (c) => {
   if (timer) {
     return;
   }
-  rl.write('\u001B[2J\u001B[0;0f');
-  rl.write('Continue the sequence!\n> ');
-  rl.write(out);
   // rl.write(null, { ctrl: true, name: 'u' });
   if ((c+'').match(/[^0-9]/)) {
     return;
   }
   const next = digits.shift();
   if (c === next) {
-    rl.write(gs + next + end);
     out += gs + next + end;
+    write(out, mistakes, max_mistakes);
+    return;
+  } else if (mistakes < max_mistakes - 1) {
+    mistakes += 1;
+    out += rs + next + end;
+    write(out,mistakes, max_mistakes);
     return;
   }
+  mistakes += 1;
   const peep = digits.slice(0,10).join('');
-  rl.write(rs + next + cs + peep + end);
   out += rs + next + cs + peep + end;
+  write(out, mistakes, max_mistakes);
   digits.unshift(...peep.split(''));
   timer = setInterval(() => {
     const ttl = 10;
     rl.write('\u001B[2J\u001B[0;0f');
-    rl.write('Continue the sequence!\n> ');
+    rl.write(`Continue the sequence! mistakes ${mistakes}/${max_mistakes}\n> `);
     rl.write(out + '\n> Resetting in ' + (ttl - count) + 's');
     if (ttl - count === 0) {
       clearInterval(timer);
